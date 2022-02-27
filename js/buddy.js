5a29db236ba6fc836c577b587c784c49d7bc8e00
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-02-27 12:30:16 +0600

    first

---
+++ js/buddy.js
@@ -0,0 +1,19 @@
+const loadBuddies=()=>{
+
+    fetch('https://randomuser.me/api/?results=5')
+    .then(res=>res.json())
+    .then(data=>displayBuddies(data) )
+}
+loadBuddies();
+const displayBuddies=data=>{
+    const buddies=data.results;
+    const buddiesDiv=document.getElementById('buddies');
+    for(const buddy of buddies){
+        console.log(buddy.email);
+        const p=document.createElement('p');
+        p.innerText=`Name:${buddy.name.first} ${buddy.name.last} ${buddy.email}`;
+        buddiesDiv.appendChild(p)
+    }
+    
+
+}
