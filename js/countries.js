24a507eadead8804763657ea7fcbb32ad85bdde1
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-02-27 23:51:40 +0600

    four

--- js/countries.js
+++ js/countries.js
@@ -9,16 +9,37 @@ loadCountries();
 const displayCountries=counties=>{
     
     const countriesDiv=document.getElementById('countries')
-    const div =document.createElement('div')
-    div.classList.add('country')
     counties.forEach(country=>{
-        const h3=document.createElement('h3');
-        h3.innerText=country.name.common;
-     div.appendChild(h3)
-     const p=document.createElement('p');
-     p.innerText=country.capital
-     div.appendChild(p)
-     countriesDiv.appendChild (div)
+        //console.log(country);
 
+        const div =document.createElement('div');
+        div.classList.add('country')
+        div.innerHTML= `
+        <h3>${country.name.common}</h3>
+        <p>${country.capital}</p>
+        <button onclick="loadCountryByName('${country.name.common}')">Details</button>
+        `;
+
+    //     const h3=document.createElement('h3');
+    //     h3.innerText=country.name.common;
+    //  div.appendChild(h3);
+    //  const p=document.createElement('p');
+    //  p.innerText=country.capital
+    //  div.appendChild(p)
+      countriesDiv.appendChild (div)
     })
+}
+const loadCountryByName=name=>{
+
+   const url=`https://restcountries.com/v3.1/name/${name}` 
+    fetch(url)
+    .then(res=>res.json())
+       .then(data=>displayCountryDetail(data[0]))
+}
+const displayCountryDetail=country=>{
+    console.log(country)
+    const countryDiv=document.getElementById('country-detail');
+    countryDiv.innerHTML=`<h5>${country.name.common}</h5>
+    <p>population${country.population}</p>
+    <img src="${country.flags.png}">`
 }
